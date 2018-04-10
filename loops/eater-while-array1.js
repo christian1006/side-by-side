5c7b27915346f7ab99a486342b8815f12b6f707f
christian1006 <37366207+christian1006@users.noreply.github.com>
2018-04-10 16:20:42 +0200

    eater-while-array1.js

---
+++ loops/eater-while-array1.js
@@ -0,0 +1,34 @@
+function eater(all_meals) {
+    let eater = [];
+    let day = 0;
+    let meal = 0;
+
+
+    while (day < 3) {
+        while (meal < 7) {
+        break
+        }
+        day++
+    }
+}
+
+
+let meals =  [
+            [
+                "cheese",
+                "tomatoes",
+                "pickles"],
+            [
+                "cheese",
+                "tomatoes",
+                "pickles"],
+            [
+                "cheese",
+                "tomatoes",
+                "pickles"
+            ]
+                
+    ]
+
+
+eater(meals)
